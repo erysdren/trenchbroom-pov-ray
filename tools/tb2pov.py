6ba9fe5759510c68744ff4c7972d89e329f9b006
erysdren <contact@erysdren.me>
2025-01-12 23:03:55 -0600

    tb2pov.py: rgb -> color and scale handling

--- tools/tb2pov.py
+++ tools/tb2pov.py
@@ -257,14 +257,17 @@ if __name__ == "__main__":
 			origin = getEntityFieldVec3(mapEntity, "origin")
 			povFile.write("light_source {\n")
 			povFile.write(f"\t<{origin.x}, {origin.y}, {origin.z}>\n")
-			if "rgb" in mapEntity:
-				color = getEntityFieldVec3(mapEntity, "rgb")
+			if "color" in mapEntity:
+				color = getEntityFieldVec3(mapEntity, "color")
 				if "scale" in mapEntity:
 					povFile.write(f"\tcolor rgb <{color.x}, {color.y}, {color.z}> * {float(mapEntity["scale"])}\n")
 				else:
 					povFile.write(f"\tcolor rgb <{color.x}, {color.y}, {color.z}>\n")
 			else:
-				povFile.write("\tcolor rgb <1, 1, 1>\n")
+				if "scale" in mapEntity:
+					povFile.write(f"\tcolor rgb <1, 1, 1> * {float(mapEntity["scale"])}\n")
+				else:
+					povFile.write("\tcolor rgb <1, 1, 1>\n")
 			povFile.write("}\n\n")
 
 	# write solids
