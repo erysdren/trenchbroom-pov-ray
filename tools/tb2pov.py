84f2c5a879e578e9214bf2b046a1944633ef92ff
erysdren <contact@erysdren.me>
2025-01-14 17:29:14 -0600

    tb2pov.py: write brush entities as a union

--- tools/tb2pov.py
+++ tools/tb2pov.py
@@ -400,14 +400,17 @@ if __name__ == "__main__":
 
 	# write solids
 	for mapEntity in mapEntities:
-		for mapBrush in mapEntity.brushes:
-			povFile.write("intersection {\n")
-			for mapBrushFace in mapBrush.faces:
-				im = Image.open(f"{mapBrushFace.texture}.png")
-				povFile.write(f"\tplane {{<{mapBrushFace.plane.x}, {mapBrushFace.plane.y}, {mapBrushFace.plane.z}>, {mapBrushFace.plane.w}")
-				povFile.write(f" texture {{ scale <{im.size[0] * mapBrushFace.scale.x}, {im.size[1] * mapBrushFace.scale.y}, 1> matrix <{mapBrushFace.u.x}, {mapBrushFace.u.y}, {mapBrushFace.u.z}, {-mapBrushFace.v.x}, {-mapBrushFace.v.y}, {-mapBrushFace.v.z}, {mapBrushFace.plane.x}, {mapBrushFace.plane.y}, {mapBrushFace.plane.z}, {-mapBrushFace.u.w}, {mapBrushFace.v.w}, {mapBrushFace.plane.w}> pigment {{ image_map {{ png \"{mapBrushFace.texture}.png\" }} }} }} }}\n")
-			povFile.write(f"\tclipped_by {{ box {{ <{mapBrush.bbox[0].x}, {mapBrush.bbox[0].y}, {mapBrush.bbox[0].z}>, <{mapBrush.bbox[1].x}, {mapBrush.bbox[1].y}, {mapBrush.bbox[1].z}> }}}}\n")
-			povFile.write("\tbounded_by { clipped_by }\n")
+		if len(mapEntity.brushes) > 0:
+			povFile.write("union {\n")
+			for mapBrush in mapEntity.brushes:
+				povFile.write("\tintersection {\n")
+				for mapBrushFace in mapBrush.faces:
+					im = Image.open(f"{mapBrushFace.texture}.png")
+					povFile.write(f"\t\tplane {{<{mapBrushFace.plane.x}, {mapBrushFace.plane.y}, {mapBrushFace.plane.z}>, {mapBrushFace.plane.w}")
+					povFile.write(f" texture {{ scale <{im.size[0] * mapBrushFace.scale.x}, {im.size[1] * mapBrushFace.scale.y}, 1> matrix <{mapBrushFace.u.x}, {mapBrushFace.u.y}, {mapBrushFace.u.z}, {-mapBrushFace.v.x}, {-mapBrushFace.v.y}, {-mapBrushFace.v.z}, {mapBrushFace.plane.x}, {mapBrushFace.plane.y}, {mapBrushFace.plane.z}, {-mapBrushFace.u.w}, {mapBrushFace.v.w}, {mapBrushFace.plane.w}> pigment {{ image_map {{ png \"{mapBrushFace.texture}.png\" }} }} }} }}\n")
+				povFile.write(f"\t\tclipped_by {{ box {{ <{mapBrush.bbox[0].x}, {mapBrush.bbox[0].y}, {mapBrush.bbox[0].z}>, <{mapBrush.bbox[1].x}, {mapBrush.bbox[1].y}, {mapBrush.bbox[1].z}> }}}}\n")
+				povFile.write("\t\tbounded_by { clipped_by }\n")
+				povFile.write("\t}\n")
 			povFile.write("}\n\n")
 
 	# clean up
